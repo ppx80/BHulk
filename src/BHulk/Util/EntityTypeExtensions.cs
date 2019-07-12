8616633f2ba583b3c8b42bedbbc96ea9dcb16d96
Fabio Michelucci <fabio.michelucci@outlook.it>
2019-07-12 21:38:08 +0200

    introductory readme

--- src/BHulk/Util/EntityTypeExtensions.cs
+++ src/BHulk/Util/EntityTypeExtensions.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace BHulk.Util
 {
     internal static class EntityTypeExtensions
     {
+        internal static string GetTableName(this IEntityType entityType) =>
+            entityType.Relational().TableName;
+
         internal static IProperty GetPrimaryKey(this IEntityType entityType) =>
             entityType.FindPrimaryKey().Properties.First();
     }
